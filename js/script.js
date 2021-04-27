7a7e978c2b85b66636fd7af04746084fbb45b1c9
Paul Valeska <35931685+Flareonn@users.noreply.github.com>
2021-04-27 14:58:03 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
-let currentLic = {
-  id: document.querySelector('.licenses-list__item[checked]'),
-  price: null,
-}
-totalResult.innerHTML = currentLic.price ? count.value * currentLic.price : null
-document.querySelector('.licenses-result__plan span:last-child').innerHTML = currentLic.id 
-  ? currentLic.id.dataset.item
-  : null
+let currentLic = {};
 
-count.addEventListener('change', ({target}) => {
-  totalResult.innerHTML = currentLic.price * target.value 
+currentLic = new Proxy(currentLic, {
+  set(target, prop, val) {
+    switch(prop){
+      case 'price':
+        target[prop] = Number(val);
+        totalResult.innerHTML = target[prop] * +count.value
+        break;
+      case 'id':
+        if(target[prop]) target[prop].removeAttribute('checked')
+        target[prop] = val;
+        if(target[prop]) {
+          target[prop].setAttribute('checked', '')
+          document.querySelector('.licenses-result__plan span:last-child').innerHTML = target[prop].dataset.item
+          currentLic.price = target[prop].querySelector('span.item__label-price span').innerHTML
+        } 
+        break;
+    }
+  }
 })
 
+currentLic.id = document.querySelector('.licenses-list__item[checked]')
+
+count.addEventListener('change', () => currentLic.price = currentLic.price)
+
 licenses.addEventListener('change', ({target}) => {
-  if(currentLic.id) currentLic.id.removeAttribute('checked');
-  currentLic.id = target.closest('.licenses-list__item');
-  currentLic.id.setAttribute('checked', '');
-    
-  document.querySelector('.licenses-result__plan span:last-child').innerHTML = currentLic.id.dataset.item
-  currentLic.price = Number(currentLic.id.querySelector('span.item__label-price span').innerHTML)
-  totalResult.innerHTML = currentLic.price * count.value
+  currentLic.id = target.closest('.licenses-list__item')
 })
